2f38732e84316e343ab2b82c7ffc5606196dfdfb
Joao Oliveira <j.oliveiradg@gmail.com>
2022-07-26 09:36:33 -0300

    feat: Adicionando formulário de cadastro e rolagem de tela

--- lib/src/auth/signup_screen.dart
+++ lib/src/auth/signup_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quitanda/src/auth/components/custom_text_field.dart';
 import 'package:quitanda/src/config/custom_colors.dart';
 
 class SignUpScreen extends StatelessWidget {
@@ -6,38 +7,86 @@ class SignUpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: CustomColors.customSwatchColor,
-      body: Column(
-        children: [
-          const Expanded(
-            child: Center(
-              child: Text(
-                'Cadastro',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 40,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              const Expanded(
+                child: Center(
+                  child: Text(
+                    'Cadastro',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                    ),
+                  ),
                 ),
               ),
-            ),
+
+              //Formulário de cadastro
+
+              Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 32,
+                    vertical: 40,
+                  ),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const CustomTextField(
+                        icon: Icons.email,
+                        label: 'Email',
+                      ),
+                      const CustomTextField(
+                        icon: Icons.lock,
+                        isSecret: true,
+                        label: 'Senha',
+                      ),
+                      const CustomTextField(
+                        icon: Icons.person,
+                        label: 'Nome',
+                      ),
+                      const CustomTextField(
+                        icon: Icons.phone,
+                        label: 'Celular',
+                      ),
+                      const CustomTextField(
+                        icon: Icons.file_copy,
+                        label: 'CPF',
+                      ),
+                      SizedBox(
+                        height: 50,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: const Text('Cadastrar Usuário',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              )),
+                        ),
+                      ),
+                    ],
+                  )),
+            ],
           ),
-
-          //Formulário de cadastro
-
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 32,
-              vertical: 40,
-            ),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(30),
-                topRight: Radius.circular(30),
-              ),
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
