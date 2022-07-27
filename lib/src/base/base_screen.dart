5d785f8a4d41c9410b64eca239a62d810c99b8f7
Joao Oliveira <j.oliveiradg@gmail.com>
2022-07-27 14:06:32 -0300

    feat: Adicionando o PageView e navegando com o BottomNavigator

--- lib/src/base/base_screen.dart
+++ lib/src/base/base_screen.dart
@@ -1,13 +1,47 @@
 import 'package:flutter/material.dart';
 
-class BaseScreen extends StatelessWidget {
-  const BaseScreen({Key? key}) : super(key: key);
+class BaseScreen extends StatefulWidget {
+  BaseScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BaseScreen> createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+  int currentIndex = 0;
+  final pageController = PageController(
+    initialPage: 0,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(color: Colors.red),
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: pageController,
+        children: [
+          Container(
+            color: Colors.red,
+          ),
+          Container(
+            color: Colors.yellow,
+          ),
+          Container(
+            color: Colors.blue,
+          ),
+          Container(
+            color: Colors.purple,
+          ),
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+            pageController.jumpToPage(index);
+          });
+        },
         type: BottomNavigationBarType.fixed,
         backgroundColor: Colors.green,
         selectedItemColor: Colors.white,
